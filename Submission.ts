42fa433cdcb0f418d48dfe3c90f6bd1b913c9de0
Jesper van den Ende <jespertheend@gmail.com>
2023-09-16 00:47:49 +0200

    initial commit

---
+++ Submission.ts
@@ -0,0 +1,29 @@
+export class Submission<T> {
+	lastModifyTime = Date.now();
+
+	refreshLifetime() {
+		this.lastModifyTime = Date.now();
+	}
+
+	get lifeTime() {
+		return Date.now() - this.lastModifyTime;
+	}
+
+	#dataSubmitted = false;
+	#resolveDataPromise: ((data: T) => void) | null = null;
+	#dataPromise = new Promise<T>((resolve) => {
+		this.#resolveDataPromise = resolve;
+	});
+
+	submitData(data: T) {
+		if (this.#dataSubmitted) return false;
+		this.#dataSubmitted = true;
+		this.refreshLifetime();
+		this.#resolveDataPromise!(data);
+		return true;
+	}
+
+	waitForData() {
+		return this.#dataPromise;
+	}
+}
---
+++ main.ts
@@ -0,0 +1,56 @@
+import { serveFile } from "https://deno.land/std@0.201.0/http/file_server.ts";
+import { Submission } from "./Submission.ts";
+
+const SUBMISSION_LIFETIME = 5 * 60 * 1000;
+
+const submissions = new Map<string, Submission<ArrayBuffer | null>>();
+
+Deno.serve(async (request) => {
+	const url = new URL(request.url);
+	const id = url.searchParams.get("id");
+	if (!id) {
+		return new Response("No id provided", { status: 404 });
+	}
+	let submission = submissions.get(id);
+	if (!submission) {
+		submission = new Submission();
+		submissions.set(id, submission);
+	}
+
+	setTimeout(() => {
+		clearOldSubmissions();
+	}, SUBMISSION_LIFETIME);
+
+	if (url.pathname == "/") {
+		submission.refreshLifetime();
+		return serveFile(request, "main.html");
+	} else if (url.pathname == "/submit" && request.method == "POST") {
+		const data = await request.arrayBuffer();
+
+		const success = submission.submitData(data);
+		if (!success) {
+			return new Response("Already submitted", { status: 400 });
+		}
+
+		return new Response(id);
+	} else if (url.pathname == "/getResult" && request.method == "GET") {
+		const result = await submission.waitForData();
+
+		return new Response(result, {
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		});
+	}
+
+	return new Response("Not found", { status: 404 });
+});
+
+function clearOldSubmissions() {
+	for (const [id, submission] of submissions) {
+		if (submission.lifeTime > SUBMISSION_LIFETIME) {
+			submission.submitData(null);
+			submissions.delete(id);
+		}
+	}
+}
